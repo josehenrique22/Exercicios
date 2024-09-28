57cede7e5660937829352da4ca645fea2df05b75
joser <joserbtgt@gmail.com>
2024-09-28 13:25:25 -0300

    Frist Commit

---
+++ src/atividades1/Ex1.java
@@ -0,0 +1,8 @@
+package atividades1;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        // Faça um programa que mostre a mensagem "Alô mundo" na tela.
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/atividades1/Ex2.java
@@ -0,0 +1,10 @@
+package atividades1;
+
+public class Ex2 {
+    // Faça um programa que exibe uma frase concatenada com a idade e o nome de um usuário (variáveis declaradas).
+    public static void main(String[] args) {
+        String nome = "John";
+        int idade = 22;
+        System.out.println("Nome: " + nome + " Idade: " + idade);
+    }
+}
